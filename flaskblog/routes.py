0e8329134e43640f911fc25f0bf6b9b6deb0d6a5
Vaishnav N T <vaishnav.nt.10@gmail.com>
2007-01-01 01:35:14 +0530

    Changed our app to work as a package

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,35 +1,7 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default='datetime.utcnow')
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return "USER: {self.title} {self.date_posted}"
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    post = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return "USER: {self.username} {self.image_file}"
-
+from flaskblog import app
+from flask import render_template, url_for, flash, redirect
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 posts = [
     {
@@ -45,7 +17,6 @@ posts = [
         'date_posted': 'july 25',
     }
 ]
-
 
 @app.route("/")
 @app.route("/home")
@@ -82,9 +53,3 @@ def login():
         else:
             flash('Login Unsuccessful, Check Credentials', 'danger')
     return render_template('login.html', title='Log-In', form=form)
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    
